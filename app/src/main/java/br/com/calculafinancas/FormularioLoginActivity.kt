7bf981e54824e9263cdbd3b0c47e71260209e438
gabrieltester <gabrielbpa10@gmail.com>
2021-06-02 22:25:16 -0300

    Inicializando o projeto

---
+++ app/src/main/java/br/com/calculafinancas/FormularioLoginActivity.kt
@@ -0,0 +1,15 @@
+package br.com.calculafinancas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormularioLoginActivity : AppCompatActivity() {
+
+    private val NOME_BAR_TELA: String = "Cadastro"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_login)
+        setTitle(NOME_BAR_TELA)
+    }
+}
